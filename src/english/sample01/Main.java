44d3da7a9500184d3c6c2f92df9de96613d6db03
智志 <@hitagi0707>
2023-07-05 22:23:27 +0900

    commit:22:23

--- src/english/sample01/Main.java
+++ src/english/sample01/Main.java
@@ -2,7 +2,7 @@ package english.sample01;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		//main
 		SampleWindow sw = new SampleWindow("english");
 		sw.setVisible(true);
 		
